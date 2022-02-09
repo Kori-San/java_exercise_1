a789b5beb6730024bba1a2545dde39d865caf5af
Kori-San <aymen@ezzayer.net>
2022-02-09 17:01:43 +0100

    Add src/Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -4,8 +4,11 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("un texte de bienvenue");
         Scanner sc = new Scanner(System.in);
-        String output = sc.nextLine();
-        if (!output.equals("quit")) {
+        while (true) {
+            String output = sc.nextLine();
+            if (output.equals("quit")) {
+                break;
+            }
             System.out.println("Unknown command");
         }
         sc.close();
