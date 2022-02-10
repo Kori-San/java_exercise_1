9ec036bb029712a2e47ed55544404b952d4ce3f0
Kori-San <aymen@ezzayer.net>
2022-02-10 13:48:29 +0100

    Add compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 javac -d bin $(find ./src/ -type f -name '*.java')
-java -cp bin ${1} #Give class name as first agrs
+if [ ${#} != 0 ]; then
+    java -cp bin ${1} #Give class name as first arg
+fi
