5eace377befe54c824bdc57d66f9c8619eb7b4e2
Kori-San <aymen@ezzayer.net>
2022-02-10 10:31:47 +0100

    Add src/Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("un texte de bienvenue");
+        System.out.println("Voici: *un texte de bienvenue*, comme demandé");
         Scanner sc = new Scanner(System.in);
         while (true) {
+            System.out.println("Enter command: ");
             String output = sc.nextLine();
             if (output.equals("quit")) {
                 break;
+            } else if (output.equals("fibo")) {
+                System.out.println("Enter index: ");
+                int index = Integer.parseInt(sc.nextLine());
+                int nP = 0;
+                int nD = 1;
+                int tmp = 0;
+                if (index != 0) {
+                    for (int n = 2; n <= index; n++) {
+                        tmp = nD;
+                        nD = nP + nD;
+                        nP = tmp;
+                    }
+                    System.out.println("Fb(" + index + ") = " + nD);
+                } else {
+                    System.out.println("Fb(0) = 0");
+                }
+            } else {
+                System.out.println("Unknown command");
             }
-            System.out.println("Unknown command");
         }
         sc.close();
     }
